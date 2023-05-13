f6aa985019ead8adb816f2bc2d3d45a331b7d4d0
Burakcan Tokses <burakcantokses5@outlook.com>
2023-05-14 01:11:23 +0300

    comment lines removed

--- FlatIcon_Desktop_Application/Managers/Request/RequestManager.cs
+++ FlatIcon_Desktop_Application/Managers/Request/RequestManager.cs
@@ -25,40 +25,6 @@ namespace FlatIcon_Desktop_Application.Managers.Request
             Accept = accept;
         }
 
-        /*public string getBearerToken(string url, string apiKey)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = RequestType.ToString();
-            request.ContentType = ContentType;
-            request.Accept = Accept;
-
-            string postData = string.Format(
-                "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n" +
-                "Content-Disposition: form-data; name=\"apikey\"\r\n\r\n" +
-                "{0}\r\n" +
-                "------WebKitFormBoundary7MA4YWxkTrZu0gW--\r\n",
-                apiKey);
-            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(postData);
-            request.ContentLength = byteArray.Length;
-
-            using (Stream dataStream = request.GetRequestStream())
-            {
-                dataStream.Write(byteArray, 0, byteArray.Length);
-            }
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            {
-                using (Stream responseStream = response.GetResponseStream())
-                {
-                    using (StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8))
-                    {
-                        string responseText = reader.ReadToEnd();
-                        return responseText;
-                    }
-                }
-            }
-        }*/
-
         public object getBearerToken(string url, string apiKey)
         {
             try
